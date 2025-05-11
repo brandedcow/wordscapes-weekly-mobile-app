a79d3d0d24d14d6e0645c3c0a6e2d8525a28bc6a
Chris Kang <chris.v.kang@gmail.com>
2025-05-10 21:42:37 -0700

    add tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="upload"
+        options={{
+          title: "Upload",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="upload" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="history" color={color} size={24} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
