5791ed6e2afa04354e9059d22d0972c4ac330a8b
Chris Kang <chris.v.kang@gmail.com>
2025-05-10 22:19:14 -0700

    add theme provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
+  const currentTheme = useColorScheme();
+  const isDarkMode = currentTheme === "dark";
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <ThemeProvider value={isDarkMode ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
